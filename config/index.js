13eaf65ab0f60e8d086f0c661c3a9158ee5485dd
jules goullée <julesgoullee@gmail.com>
2018-06-03 01:12:46 +0200

    deploy

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export default {
   ENV: process.env.ENV || process.env.VUE_APP_ENV || 'development',
   CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS || process.env.VUE_APP_CONTRACT_ADDRESS
-  || '0xf11d466eb6d4a9d9fa65163ba88c729a30cec9fe'
+  || '0x65874567e5702d32f22164d391407686b72430c0'
 };
