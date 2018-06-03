ebab079908b911cfa6fd7c27c6e664994d249c51
jules goullée <julesgoullee@gmail.com>
2018-06-03 04:55:24 +0200

    header link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="navbar-hackathon">
     <!-- Logo -->
-    <a href="/" class="navbar-hackathon-brand">
+    <a href="/#/" class="navbar-hackathon-brand">
       <img src="images/logo.png" alt="logo">
     </a>
     <!-- Right Navigation -->
     <div class="navbar-hackathon-right hidden-xs hidden-sm">
-      <a href="" class="navbar-hackathon-item navbar-hackathon-link">Voting petitions</a>
+      <a href="/#/" class="navbar-hackathon-item navbar-hackathon-link">Voting petitions</a>
       <a href="" class="navbar-hackathon-item navbar-hackathon-link">Petitions validated</a>
       <a href="" class="navbar-hackathon-item navbar-hackathon-link">Support us</a>
-      <a href="" class="navbar-hackathon-item navbar-hackathon-link btn-navbar"> Lauch my petition </a>
+      <a href="/#/add" class="navbar-hackathon-item navbar-hackathon-link btn-navbar"> Lauch my petition </a>
       <!-- Profile picture with dropdown list -->
       <div class="navbar-hackathon-item">
         <div class="dropdown">
